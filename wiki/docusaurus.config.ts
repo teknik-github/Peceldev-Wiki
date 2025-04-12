be5b770a58b0cac9aaf28070f0d2a68b5ce6c96a
teknik-github <mohamatraffi170@gmail.com>
2025-04-12 16:38:34 +0000

    add

--- wiki/docusaurus.config.ts
+++ wiki/docusaurus.config.ts
@@ -141,7 +141,7 @@ const config: Config = {
       //     ],
       //   },
       // ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Peceldev Wiki, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
